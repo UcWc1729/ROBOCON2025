734d031ff66554e710acc8c77e3c5189ea0173c6
zxh <2253991561@qq.com>
2025-06-18 18:27:28 +0800

    Initial commit: 新增运球机器的底盘运动控制代码A_f427_motor和投篮机器底盘运动控制代码A_f427_motor_2

---
+++ A_f427_motor/MDK-ARM/A_f427_motor/drv_math.d
@@ -0,0 +1,6 @@
+a_f427_motor\drv_math.o: ..\User\drv_math.cpp
+a_f427_motor\drv_math.o: ..\User\drv_math.h
+a_f427_motor\drv_math.o: D:\C51\ARM\ARMCC\Bin\..\include\stdint.h
+a_f427_motor\drv_math.o: D:\C51\ARM\ARMCC\Bin\..\include\limits.h
+a_f427_motor\drv_math.o: D:\C51\ARM\ARMCC\Bin\..\include\math.h
+a_f427_motor\drv_math.o: D:\C51\ARM\ARMCC\Bin\..\include\float.h
---
+++ A_f427_motor_2/MDK-ARM/A_f427_motor/alg_pid.d
@@ -0,0 +1,7 @@
+a_f427_motor\alg_pid.o: ..\User\alg_pid.cpp
+a_f427_motor\alg_pid.o: ..\User\alg_pid.h
+a_f427_motor\alg_pid.o: ..\User\drv_math.h
+a_f427_motor\alg_pid.o: D:\C51\ARM\ARMCC\Bin\..\include\stdint.h
+a_f427_motor\alg_pid.o: D:\C51\ARM\ARMCC\Bin\..\include\limits.h
+a_f427_motor\alg_pid.o: D:\C51\ARM\ARMCC\Bin\..\include\math.h
+a_f427_motor\alg_pid.o: D:\C51\ARM\ARMCC\Bin\..\include\float.h
